696da5dffda990e7407aec9b67fb15d7c820f24b
BrivaHamisi <brivahamisi@gmail.com>
2024-08-22 12:30:44 +0300

    Functioning CRUD using Prisma and Postgress Online DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PostService } from './post.service';
 
 @Module({
   imports: [PrismaModule, ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PostService],
 })
 export class AppModule {}
